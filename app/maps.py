ee58488f8a8903f3d8e053033c8b25a8b7e10ddb
mehdi.bichari <mehdi.bichari@outscale.com>
2020-06-15 14:34:22 +0200

    Change gmaps api to requests

--- app/maps.py
+++ app/maps.py
@@ -1,6 +1,5 @@
+import requests
 from typing import Dict, List
-
-import googlemaps
 
 
 def get_location(questions: List) -> List[Dict]:
@@ -11,11 +10,20 @@ def get_location(questions: List) -> List[Dict]:
 
     :rtype List[Dict]
     """
-    gmaps = googlemaps.Client(key='AIzaSyAI6WPMSEM0YvN81wqDaZsnbNmKNldwe_4')
+    results = [
+        requests.get(
+            "https://maps.googleapis.com/maps/api/geocode/json",
+            {
+                'address': address,
+                'key': 'AIzaSyAI6WPMSEM0YvN81wqDaZsnbNmKNldwe_4'
+            }
+        ).json()
+        for address in questions
+    ]
 
-    results: List = [gmaps.geocode(question) for question in questions]
     coordonates = [
-        result[0]['geometry']['location'] for result in results if result
+        result['results'][0]['geometry']['location'] for result in results
+        if result
     ]
 
     for coordonate in coordonates:
