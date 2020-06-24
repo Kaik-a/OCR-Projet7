47cf2a4800f7880d25e9fcc7591da3f7dedf7094
mehdi.bichari <mehdi.bichari@outscale.com>
2020-06-24 13:05:15 +0200

    Fixed marker bug and file import

--- app/wiki.py
+++ app/wiki.py
@@ -1,13 +1,13 @@
 """Media wiki interface"""
+import re
 from difflib import get_close_matches
 from random import choice
-import re
-import requests
 from typing import Dict, List, Union
 
+import requests
 
-def get_info_on_loc(locations: List[Dict],
-                    parsed: List[Dict]) -> Union[str, List]:
+
+def get_info_on_loc(locations: List[Dict], parsed: List[Dict]) -> Union[str, List]:
     """
     Get information of a place by using its location.
 
@@ -18,47 +18,44 @@ def get_info_on_loc(locations: List[Dict],
         requests.get(
             "https://fr.wikipedia.org/w/api.php",
             {
-             "action": "query",
-             "list": "geosearch",
-             "format": "json",
-             "gscoord": f"{location['lat']}|{location['lng']}"
-            }).json()['query']['geosearch'] for location in locations
+                "action": "query",
+                "list": "geosearch",
+                "format": "json",
+                "gscoord": f"{location['lat']}|{location['lng']}",
+            },
+        ).json()["query"]["geosearch"]
+        for location in locations
     ]
     possible_matches: List = [
-        [match['title'] for match in result] for result in results
+        [match["title"] for match in result] for result in results
     ]
     if len(results) == 1:
-        best_match: str = get_close_matches(
-            parsed[0],
-            possible_matches[0],
-            1
-        )[0]
+        best_match: str = get_close_matches(parsed[0], possible_matches[0], 1)
+        if best_match:
+            best_match = best_match[0]
+        else:
+            best_match = possible_matches[0][0]
+
         return get_summary(
-            [
-                result for result in results[0]
-                if result['title'] == best_match
-            ].pop()
+            [result for result in results[0] if result["title"] == best_match].pop()
         )
     elif not results:
-        print("Quoi!? C'est quoi ce charabia? Je comprends rien à votre "
-              "language de jeuns, tu peux poser ta question correctement ?")
+        print(
+            "Quoi!? C'est quoi ce charabia? Je comprends rien à votre "
+            "language de jeuns, tu peux poser ta question correctement ?"
+        )
     else:
         i = 0
         best_matches = []
         for parse in parsed:
-            best_match = get_close_matches(
-                    parse, possible_matches[i], 1
-                )
+            best_match = get_close_matches(parse, possible_matches[i], 1)
             if best_match:
                 best_matches.append(best_match[0])
             else:
                 best_matches.append(possible_matches[i][0])
             i += 1
 
-        places_found = [
-            (index, result)
-            for index, result in enumerate(best_matches)
-        ]
+        places_found = [(index, result) for index, result in enumerate(best_matches)]
         return get_summary(multiple_choices(places_found))
 
 
@@ -70,16 +67,18 @@ def get_summary(page: Dict) -> str:
     :return: str summary
     """
     fullpage = requests.get(
-        'https://fr.wikipedia.org/w/api.php',
-        {'action': 'query',
-         'format': 'json',
-         'prop': 'extracts',
-         'titles': f"{page['title']}"}
+        "https://fr.wikipedia.org/w/api.php",
+        {
+            "action": "query",
+            "format": "json",
+            "prop": "extracts",
+            "titles": f"{page['title']}",
+        },
     )
-    fulltext = fullpage.json()['query']['pages'][f'{page["pageid"]}']['extract']
-    summary = fulltext.split('<h2>')[0]
+    fulltext = fullpage.json()["query"]["pages"][f'{page["pageid"]}']["extract"]
+    summary = fulltext.split("<h2>")[0]
     pattern = "([<].*?[>])"
-    return re.sub(pattern, '', summary)
+    return re.sub(pattern, "", summary)
 
 
 def endow(summary: str) -> str:
@@ -118,7 +117,9 @@ def multiple_choices(choices: List) -> Dict:
     except ValueError:
         pass
 
-    print(f"Tu as tapé {answer}, tu vois bien que ça fait pas partie"
-          f" des choix proposés? De mon temps, on savait lire au moins!")
+    print(
+        f"Tu as tapé {answer}, tu vois bien que ça fait pas partie"
+        f" des choix proposés? De mon temps, on savait lire au moins!"
+    )
 
     return multiple_choices(choices)
